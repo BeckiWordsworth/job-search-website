a6fb240cb817c8c552030c48966a869c7e0bd97f
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-02-20 23:09:10 +0100

    Couple more helpers I will deffo need

--- helpers.php
+++ helpers.php
@@ -39,7 +39,6 @@ function loadView($name)
  * 
  */
 
-
 function loadPartial($name)
 {
   $partialPath = basePath("views/partials/{$name}.php");
@@ -49,4 +48,35 @@ function loadPartial($name)
   } else {
     echo "View '{$name} not found!'";
   }
+}
+
+/**
+ * Inspect a value/s
+ * 
+ * @param mixed $value
+ * @return void
+ * 
+ */
+
+function inspect($value)
+{
+  echo "<pre>";
+  var_dump($value);
+  echo "<pre>";
+}
+
+/**
+ * Inspect a value/s and die
+ * 
+ * @param mixed $value
+ * @return void
+ * 
+ */
+
+function inspecAndDie($value)
+{
+  echo "<pre>";
+  di(var_dump($value));
+  echo "<pre>";
+
 }
