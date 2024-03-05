0a363055c3969f863d407a1c6a12b03cc79128f1
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-03-05 23:23:03 +0100

    Adding flash messages for error and delete

--- public/index.php
+++ public/index.php
@@ -1,16 +1,11 @@
 <?php
+session_start();
 require __DIR__ . "/../vendor/autoload.php";
 require "../helpers.php";
 
 use Framework\Router;
 
-//Custom Autoloader - require files with the same name as the class 
-// spl_autoload_register(function ($class) {
-//   $path = basePath("Framework/" . $class . ".php");
-//   if (file_exists($path)) {
-//     require $path;
-//   };
-// });
+
 
 // Instantiating the router
 $router = new Router();
