83c030284238e0dd85a0dcb314cbe7fed5da665b
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-03-13 22:45:37 +0100

    User controllers

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -14,4 +14,27 @@ class UserController
     $config = require basePath("config/db.php");
     $this->db = new Database($config);
   }
+
+
+  /**
+   * Show the login page
+   * 
+   * @return void
+   */
+
+  public function login()
+  {
+    loadView("users/login");
+  }
+
+  /**
+   * Show the register page
+   * 
+   * @return void
+   */
+
+  public function create()
+  {
+    loadView("users/create");
+  }
 }
